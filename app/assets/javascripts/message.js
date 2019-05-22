9b06f54bfe25eee60180b8c45f2847b827e9afd0
S <42575166+shinpei555@users.noreply.github.com>
2019-05-22 18:59:22 +0900

    message.jsのhtml更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,24 @@
 $(function() {
+  function buildHTML(message){
+    
+  var html = `<div class="message">
+              <div class="upper-message">
+                <div class="upper-message__user-name">
+                  ${message.name}
+                </div>
+                <div class="upper-message__date">
+                    ${message.date}
+                </div>
+              </div>
+              <div class="lower-meesage">
+                <p class="lower-message__content">
+                  ${message.content}
+                </p>
+                ${message.image}
+              </div>
+            </div>`;
+  return html
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,5 +30,17 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
-  });
+  })
+  .done(function(data){
+    console.log(data);
+    var html = buildHTML(data);
+    $('.form__submit').prop('disabled', false);
+    $('.messages').append(html);
+    $('.form__message').val('');
+    $('.hidden').val('');
+  })
+  .fail(function(){
+    alert('error');
+  })
+ });
 });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,6 @@
+  
+  json.content  @message.content
+  json.image    @message.image
+  json.name     @message.user.name
+  json.id       @message.id
+  json.date     @message.created_at.strftime('%Y/%m/%d %R')

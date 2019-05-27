e9015b5a775f70fab462a564099295350b0f9e6b
S <42575166+shinpei555@users.noreply.github.com>
2019-05-27 20:35:16 +0900

    デバッグツールの削除

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     respond_to do |format|
       format.html
       format.json{@messages = Message.where('id > ?',params[:id]) }
